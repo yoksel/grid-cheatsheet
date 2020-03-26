ba879793264e01904bdb9879dddf367f01bf5aca
yoksel <yokselzok@gmail.com>
2020-03-26 17:07:39 +0300

    Clear code a little bit

--- src/app.js
+++ src/app.js
@@ -8,16 +8,12 @@ import './scss/styles.scss';
 
 const doc = document;
 const $ = tinyLib;
-const aside = doc.querySelector('.l-aside');
 
-
-console.log(new Nav({
+new Nav({
   groups,
   data,
-  targetElem: aside
-}))
-
-
+  targetElem: doc.querySelector('.l-aside')
+});
 
 const body = $.get('body');
 const main = $.get('.l-main');
@@ -27,10 +23,6 @@ const navHolder = $.create('ul').addClass('nav');
 const contentHolder = $.create('div').addClass('content');
 const stylesHolder = $.create('style').attr({'id':'grid'});
 
-const navMarker = $.create('li').addClass('nav__marker');
-// const projectLinks = $.create('li')
-//                     .addClass(['nav__item', 'nav__item--project-links'])
-//                     .append( $.get('.project-links') );
 
 const sections = [];
 const navItems = {};
@@ -49,4 +41,4 @@ const demoElemClasses = {
 const navItemCurrentClass = 'nav__item--current';
 const localStorThemeKey = 'fbchTheme';
 
-console.log('hello')
+// console.log('hello')
