be2b57a37bff37a5d3d1af7cfcd8ab360a1b9621
yoksel <yokselzok@gmail.com>
2020-03-28 10:45:38 +0300

    Add autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = () => {
           use: [
             'style-loader',
             'css-loader',
+            'postcss-loader',
             'sass-loader',
           ]
         },
