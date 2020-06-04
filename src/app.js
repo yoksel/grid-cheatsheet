c2184b91a3f9dcd6df9b01eaacfb56088c802434
yoksel <yokselzok@gmail.com>
2020-06-04 14:35:55 +0300

    Improve highlight of invalid links

--- src/app.js
+++ src/app.js
@@ -81,20 +81,31 @@ navItems.reduce = [].reduce;
 
 // ---------------------------------------------
 
-function checkLinks() {
+function checkLinks () {
   const elemsWithId = Array.from(document.querySelectorAll('[id]'));
   const elemsById = elemsWithId.reduce((prev, item) => {
     prev[item.id] = item;
     return prev;
-  }, {})
-  const links = document.querySelectorAll('a[href^="#"]');
+  }, {});
+  const links = document.querySelectorAll('a[href*="#"]');
+  let counter = 1;
 
   links.forEach((link) => {
-    const hrefTail = link.getAttribute('href').slice(1);
-    if (!elemsById[hrefTail]) {
-      console.log(hrefTail);
-      console.log('\n');
+    const href = link.getAttribute('href');
+    const isExternal = href.includes('http');
+    const hrefTail = link.getAttribute('href').split('#')[1];
+
+    if (isExternal) {
+      if (elemsById[hrefTail] && href.includes('css3-grid-layout')) {
+        console.log('external, id exist in doc: ', hrefTail);
+        link.classList.add('marked-link');
+}
+    } else if (!elemsById[hrefTail]) {
+      console.log(`id not exist ${hrefTail}`);
       link.classList.add('marked-link');
+      counter++;
     }
   });
+
+  console.log(`Total: ${counter}`);
 }
