6b5e6489739767a71adceb3d40e00d6e00d1048d
yoksel <yokselzok@gmail.com>
2020-06-19 12:31:22 +0300

    Move gap values from file to row-column-gap.js

--- src/js/data/props/spacing/row-column-gap.js
+++ src/js/data/props/spacing/row-column-gap.js
@@ -1,6 +1,5 @@
 import columnGap from './column-gap';
 import rowGap from './row-gap';
-import gapValues from './gap-values';
 
 export default {
   name: 'row-gap, column-gap',
@@ -29,14 +28,25 @@ Such additional space effectively increases the size of these <a href="#gutter">
 
   values: [
     {
-      name: 'normal | &lt;length-percentage>',
-      alias: 'row-column-gap-values',
-      desc: ''
-    }
-  ],
+      name: '&lt;length-percentage>',
+      alias: 'typedef-length-percentage',
+      desc: `<p class="">Specifies a gap between “rows” or “columns”,
+as defined by the layout modes to which it applies;
+see subsections below for details.</p>
+     <p>Negative values are invalid. <a href="https://www.w3.org/TR/css-sizing-3/#percentage-sizing">As in the min size properties and margins/paddings</a> <a href="https://www.w3.org/TR/css-grid-1/#biblio-css-sizing-3">[CSS-SIZING-3]</a>,
+percentages resolve against zero
+for determining intrinsic size contributions,
+but resolve against the box’s content box
+when laying out the box’s contents.</p>`
+    },
 
-  children: [
-    gapValues
+    {
+      name: 'normal',
+      alias: 'valdef-row-gap-normal',
+      desc: `<p>The <a href="#valdef-row-gap-normal">normal</a> represents
+a used value of <code>1em</code> on <a href="https://www.w3.org/TR/css3-multicol/#multi-column-container">multi-column containers</a>,
+and a used value of <code>0px</code> in all other contexts.</p>`
+    }
   ],
 
   demos: [
